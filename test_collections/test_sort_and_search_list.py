48b3f83d8cb93667ad88d1d5c5370eaeec9273dd
G8A4W0416 <wilhelmg1@gmail.com>
2020-03-04 23:25:02 -0600

    Module 7 Topic 1 Search and Sort List Assignment third commit with sort_list() test failing.

--- test_collections/test_sort_and_search_list.py
+++ test_collections/test_sort_and_search_list.py
@@ -12,6 +12,11 @@ class MyTestCase(unittest.TestCase):
     def test_search_list_item_not_found(self, mock_input):
         self.assertEqual(sort_and_search_list.search_list([1, 4, 5, 36, 99]), -1)
 
+    def test_sort_list(self):
+        unsorted_list = [99, 1, 4, 5, 36]
+        sorted_list = [1, 4, 5, 36, 99]
+        self.assertEqual(sort_and_search_list.sort_list(unsorted_list), sorted_list)
+
 
 if __name__ == '__main__':
     unittest.main()
