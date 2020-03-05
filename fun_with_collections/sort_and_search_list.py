be16b0ebd1c52a10ce3d17d1aa1353262768809d
G8A4W0416 <wilhelmg1@gmail.com>
2020-03-04 23:35:49 -0600

    Module 7 Topic 1 Search and Sort List Assignment final commit with sort_list() test passing.

--- fun_with_collections/sort_and_search_list.py
+++ fun_with_collections/sort_and_search_list.py
@@ -1,5 +1,9 @@
-def sort_list():
-    pass
+def sort_list(list_to_sort):
+    list_to_sort.sort()
+    return list_to_sort
+    # this is returning the sorted array. I included the return to be able to assert that the expected array and the
+    # actual array are equal
+
 
 
 def search_list(list_to_search):
--- test_collections/test_sort_and_search_list.py
+++ test_collections/test_sort_and_search_list.py
@@ -6,16 +6,16 @@ from fun_with_collections import sort_and_search_list
 class MyTestCase(unittest.TestCase):
     @patch('builtins.input', return_value=36)
     def test_search_list_item_found(self, mock_input):
-        self.assertEqual(sort_and_search_list.search_list([1, 4, 5, 36, 99]), 3)
+        self.assertEqual(3, sort_and_search_list.search_list([1, 4, 5, 36, 99]))
 
     @patch('builtins.input', return_value=100)
     def test_search_list_item_not_found(self, mock_input):
-        self.assertEqual(sort_and_search_list.search_list([1, 4, 5, 36, 99]), -1)
+        self.assertEqual(-1, sort_and_search_list.search_list([1, 4, 5, 36, 99]))
 
     def test_sort_list(self):
         unsorted_list = [99, 1, 4, 5, 36]
         sorted_list = [1, 4, 5, 36, 99]
-        self.assertEqual(sort_and_search_list.sort_list(unsorted_list), sorted_list)
+        self.assertEqual(sorted_list, sort_and_search_list.sort_list(unsorted_list))
 
 
 if __name__ == '__main__':
