3cfb5e1d5ee1e2747d2182d66cfbe6f3d75a290e
UNIVERSE\gwilhelm <gwilhelm@cds-global.com>
2020-03-04 18:50:34 -0600

    Module 7 Topic 1 Basic List Assignment second commit to get test to pass.

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,6 +1,15 @@
-def make_list():
-    pass
-
-
 def get_input():
     pass
+
+
+def make_list():
+    inputs = []
+
+    for i in range(3):
+        try:
+            value = int(get_input())
+            inputs.insert(i, value)
+        except ValueError:
+            print("Invalid type.")
+
+    return inputs
