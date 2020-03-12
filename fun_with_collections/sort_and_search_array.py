5bcd93fe9c51213e9bf7f4d23c086e22b10be3ea
G8A4W0416 <wilhelmg1@gmail.com>
2020-03-11 23:59:43 -0500

    Module 7 Topic 4 Search and Sort Array Assignment initial commit with test_search_array_item_found() and test_search_array_item_not_found() tests failing.

---
+++ fun_with_collections/sort_and_search_array.py
@@ -0,0 +1,6 @@
+def sort_array(list_to_sort):
+    pass
+
+
+def search_array(list_to_search):
+    pass
---
+++ test_collections/test_sort_and_search_array.py
@@ -0,0 +1,23 @@
+import unittest
+import array as arr
+from unittest.mock import patch
+from fun_with_collections import sort_and_search_array
+
+
+class MyTestCase(unittest.TestCase):
+    @patch('builtins.input', return_value=36)
+    def test_search_array_item_found(self, mock_input):
+        self.assertEqual(3, sort_and_search_array.search_list([1, 4, 5, 36, 99]))
+
+    @patch('builtins.input', return_value=100)
+    def test_search_array_item_not_found(self, mock_input):
+        self.assertEqual(-1, sort_and_search_array.search_list([1, 4, 5, 36, 99]))
+
+    def test_sort_array(self):
+        unsorted_list = [99, 1, 4, 5, 36]
+        sorted_list = [1, 4, 5, 36, 99]
+        self.assertEqual(sorted_list, sort_and_search_array.sort_list(unsorted_list))
+
+
+if __name__ == '__main__':
+    unittest.main()
