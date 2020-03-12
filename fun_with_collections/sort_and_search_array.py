f1579ee66f700e0c86f663c376f004597db66ed0
G8A4W0416 <wilhelmg1@gmail.com>
2020-03-12 00:27:28 -0500

    Module 7 Topic 4 Search and Sort Array Assignment third commit with search_array function completed so the two search tests pass.

--- fun_with_collections/sort_and_search_array.py
+++ fun_with_collections/sort_and_search_array.py
@@ -1,6 +1,14 @@
+import array as arr
 def sort_array(list_to_sort):
     pass
 
 
-def search_array(list_to_search):
-    pass
+def search_array(array_to_search):
+    user_input = input("What is the item you are searching for? ")
+
+    try:
+        index = array_to_search.index(user_input)
+    except ValueError:
+        index = -1
+
+    return index
--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -7,16 +7,11 @@ from fun_with_collections import sort_and_search_array
 class MyTestCase(unittest.TestCase):
     @patch('builtins.input', return_value=36)
     def test_search_array_item_found(self, mock_input):
-        self.assertEqual(3, sort_and_search_array.search_array([1, 4, 5, 36, 99]))
+        self.assertEqual(3, sort_and_search_array.search_array(arr.array('I', [1, 4, 5, 36, 99])))
 
     @patch('builtins.input', return_value=100)
     def test_search_array_item_not_found(self, mock_input):
-        self.assertEqual(-1, sort_and_search_array.search_array([1, 4, 5, 36, 99]))
-
-    def test_sort_array(self):
-        unsorted_list = [99, 1, 4, 5, 36]
-        sorted_list = [1, 4, 5, 36, 99]
-        self.assertEqual(sorted_list, sort_and_search_array.sort_array(unsorted_list))
+        self.assertEqual(-1, sort_and_search_array.search_array(arr.array('I', [1, 4, 5, 36, 99])))
 
 
 if __name__ == '__main__':
